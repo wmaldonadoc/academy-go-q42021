76ae6a647ff51abd31e97c13cff0399910b28c8f
weyler.maldonado <weyler.maldonado@gmail.com>
2021-11-05 18:27:23 -0600

    refactor(*): make zap as global logger

--- infrastructure/router/router.go
+++ infrastructure/router/router.go
@@ -3,6 +3,7 @@ package router
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/wmaldonadoc/academy-go-q42021/config"
+	"go.uber.org/zap"
 )
 
 func NewRouter() {
@@ -10,6 +11,7 @@ func NewRouter() {
 	router := gin.Default()
 
 	router.GET("/ping", func(c *gin.Context) {
+		zap.S().Info("Testing global")
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
--- main.go
+++ main.go
@@ -3,10 +3,26 @@ package main
 import (
 	"github.com/wmaldonadoc/academy-go-q42021/infrastructure/router"
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
+func bootingGlobalLogger() *zap.Logger {
+	// Replacing logger global
+	config := zap.NewDevelopmentConfig()
+	config.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+	config.EncoderConfig.TimeKey = "timestamp"
+	config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	logger, _ := config.Build()
+	return logger
+
+}
+
 func main() {
-	logger, _ := zap.NewProduction()
-	logger.Info("Booting router...")
+	loggerMgr := bootingGlobalLogger()
+	zap.ReplaceGlobals(loggerMgr)
+	defer loggerMgr.Sync() // flushes buffer, if any
+	logger := loggerMgr.Sugar()
+
+	logger.Debug("Booting routes...")
 	router.NewRouter()
 }
