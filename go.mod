6b77c7ea1b0a5bb0c59ea264489648c682d9cef2
weyler.maldonado <weyler.maldonado@gmail.com>
2021-12-02 18:16:33 -0600

    fix test

--- go.mod
+++ go.mod
@@ -5,15 +5,16 @@ go 1.17
 require (
 	github.com/bxcodec/faker/v3 v3.6.0
 	github.com/gin-gonic/gin v1.7.4
-	github.com/go-playground/assert/v2 v2.0.1
 	github.com/go-playground/validator/v10 v10.9.0
 	github.com/joho/godotenv v1.4.0
 	github.com/onsi/ginkgo v1.16.5
 	github.com/onsi/gomega v1.17.0
+	github.com/stretchr/testify v1.7.0
 	go.uber.org/zap v1.19.1
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
@@ -25,6 +26,8 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/nxadm/tail v1.4.8 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.1.0 // indirect
 	github.com/ugorji/go/codec v1.2.6 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
@@ -35,4 +38,5 @@ require (
 	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- interface/controller/pokemon_controller_test.go
+++ interface/controller/pokemon_controller_test.go
@@ -1,1 +1,67 @@
 package controller
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"testing"
+
+	"github.com/bxcodec/faker/v3"
+	"github.com/gin-gonic/gin"
+	"github.com/wmaldonadoc/academy-go-q42021/domain/model"
+	"github.com/wmaldonadoc/academy-go-q42021/infrastructure/api"
+	"github.com/wmaldonadoc/academy-go-q42021/interface/presenter"
+	"github.com/wmaldonadoc/academy-go-q42021/interface/repository"
+	"github.com/wmaldonadoc/academy-go-q42021/usecase/interactor"
+	"github.com/wmaldonadoc/academy-go-q42021/workers"
+)
+
+func TestGetPokemonById(t *testing.T) {
+	var pokemons []*model.Pokemon
+	ctx, _ := gin.CreateTestContext(httptest.NewRecorder())
+	pokemon := model.Pokemon{}
+	for i := 1; i <= 10; i++ {
+		pokemon.ID = i
+		pokemon.Ability = faker.Word()
+		pokemon.Name = faker.Name()
+		pokemons = append(pokemons, &pokemon)
+	}
+	rp := repository.NewPokemonRepository(pokemons)
+	pr := presenter.NewPokemonPresenter()
+	api := api.NewApiClient(&http.Client{})
+	dispatcher := workers.NewDispatcher()
+
+	tests := []struct {
+		name       string
+		HTTPStatus int
+		keyParam   string
+		valueParam string
+	}{
+		{name: "Return a successfull response", HTTPStatus: 200, keyParam: "id", valueParam: "10"},
+		{name: "Return a bad request response", HTTPStatus: 422, keyParam: "test", valueParam: "4"},
+		{name: "Return a not found response", HTTPStatus: 404, keyParam: "id", valueParam: "2"},
+	}
+
+	for _, test := range tests {
+		ctx.Params = []gin.Param{
+			{
+				Key:   test.keyParam,
+				Value: test.valueParam,
+			},
+		}
+
+		pi := interactor.NewPokemonInteractor(
+			rp,
+			pr,
+			api,
+			dispatcher,
+		)
+		ctrl := NewPokemonController(pi)
+		resp := ctrl.GetByID(ctx)
+
+		if !reflect.DeepEqual(resp.HTTPStatus, test.HTTPStatus) {
+			t.Errorf("%s: Expected %d but got %d", test.name, test.HTTPStatus, resp.HTTPStatus)
+		}
+
+	}
+}
